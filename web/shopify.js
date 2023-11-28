404171df74254f5d4db5e966c7fe4ba418c76962
minhnb <minhnb@demetio.com>
2023-11-28 10:08:51 +0700

    Update shopify app

--- web/shopify.js
+++ web/shopify.js
@@ -19,6 +19,11 @@ const billingConfig = {
 const shopify = shopifyApp({
   api: {
     apiVersion: LATEST_API_VERSION,
+    apiKey: process.env.SHOPIFY_API_KEY,
+    apiSecretKey: process.env.SHOPIFY_API_SECRET,
+    scopes: process.env.SCOPES,
+    hostName: process.env.HOST,
+    hostScheme: "https",
     restResources,
     billing: undefined, // or replace with billingConfig above to enable example billing
   },
